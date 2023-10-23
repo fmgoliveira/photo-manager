a242e51d6212caf2a257a58a589dd8210879a796
dr-monocle <francisco@franciscoliveira.com>
2023-10-23 17:40:25 +0100

    Color to argb

--- mobile/lib/constants/immich_colors.dart
+++ mobile/lib/constants/immich_colors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 Color immichBackgroundColor = const Color(0xFFf6f8fe);
-Color immichDarkBackgroundColor = const Color(0x1e1f22ff);
+Color immichDarkBackgroundColor = const Color.fromARGB(255, 30, 31, 34);
 Color immichDarkThemePrimaryColor = const Color.fromARGB(255, 103, 203, 250);
