5530d2c694636f7ade91ee12fbfd356726849a4b
Francisco Oliveira <francisco@franciscoliveira.com>
2023-10-23 08:10:11 +0100

    Update immich_colors.dart

--- mobile/lib/constants/immich_colors.dart
+++ mobile/lib/constants/immich_colors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 Color immichBackgroundColor = const Color(0xFAF9F6fe);
-Color immichDarkBackgroundColor = const Color(0x202124ff);
-Color immichDarkThemePrimaryColor = const Color(0x649fe3ff);
+Color immichDarkBackgroundColor = const Color.fromARGB(255, 0, 0, 0);
+Color immichDarkThemePrimaryColor = const Color.fromARGB(255, 103, 203, 250);
