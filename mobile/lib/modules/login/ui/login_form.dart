e7ece84c54a948e14e0eb7fc87258e264f11c001
fmgoliveira <francisco2@franciscoliveira.com>
2023-10-23 07:17:26 +0100

    Updated layout x2

--- mobile/lib/modules/login/ui/login_form.dart
+++ mobile/lib/modules/login/ui/login_form.dart
@@ -127,9 +127,9 @@ class LoginForm extends HookConsumerWidget {
     );
 
     populateTestLoginInfo() {
-      usernameController.text = 'testuser@email.com';
-      passwordController.text = 'password';
-      serverEndpointController.text = 'http://10.1.15.216:2283/api';
+      // usernameController.text = 'testuser@email.com';
+      // passwordController.text = 'password';
+      serverEndpointController.text = 'https://pics.franciscoliveira.com/api';
     }
 
     login() async {
@@ -391,11 +391,8 @@ class LoginForm extends HookConsumerWidget {
                     children: [
                       GestureDetector(
                         onDoubleTap: () => populateTestLoginInfo(),
-                        child: RotationTransition(
-                          turns: logoAnimationController,
-                          child: const ImmichLogo(
-                            heroTag: 'logo',
-                          ),
+                        child: const ImmichLogo(
+                          heroTag: 'logo',
                         ),
                       ),
                       const ImmichTitleText(),
--- mobile/lib/shared/views/version_announcement_overlay.dart
+++ mobile/lib/shared/views/version_announcement_overlay.dart
@@ -53,7 +53,7 @@ class VersionAnnouncementOverlay extends HookConsumerWidget {
                                 fontSize: 16,
                                 fontFamily: 'WorkSans',
                                 fontWeight: FontWeight.bold,
-                                color: Colors.indigo,
+                                color: Colors.blue,
                               ),
                             ).tr(),
                             Padding(
@@ -75,8 +75,8 @@ class VersionAnnouncementOverlay extends HookConsumerWidget {
                                     const TextSpan(
                                       text: ' Pics ',
                                       style: TextStyle(
-                                        fontFamily: "SnowBurstOne",
-                                        color: Colors.indigo,
+                                        fontFamily: "WorkSans",
+                                        color: Colors.blue,
                                         fontWeight: FontWeight.bold,
                                       ),
                                     ),
@@ -110,7 +110,7 @@ class VersionAnnouncementOverlay extends HookConsumerWidget {
                                 style: ElevatedButton.styleFrom(
                                   shape: const StadiumBorder(),
                                   visualDensity: VisualDensity.standard,
-                                  backgroundColor: Colors.indigo,
+                                  backgroundColor: Colors.blue,
                                   foregroundColor: Colors.grey[50],
                                   elevation: 2,
                                   padding: const EdgeInsets.symmetric(
